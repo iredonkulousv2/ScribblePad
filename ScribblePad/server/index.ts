f350d9fb44b8e6dc09babbcdaf9424a98f1ccfda
Phil <phil.luc89@gmail.comm>
2023-06-16 13:56:39 -0400

    added env

--- ScribblePad/server/index.ts
+++ ScribblePad/server/index.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response} from 'express'
 import mongoose from 'mongoose'
+import {config} from 'dotenv'
 
+config()
 import User from './models/user'
 
 const app = express();
-const PORT = 8000
+const PORT = process.env.PORT
 
 app.use(express.json())
 
 app.post('/login', async (req: Request, res: Response) => {
   
   const newUser = new User({
-    _id: 1,
+    _id: 2,
     name: 'Jack',
     password: 'Jack'
   })
@@ -27,11 +29,13 @@ app.get('/', (req: Request,res: Response) => {
 
 //const db = mongoose.connect('mongodb+srv://philip14633:philip14633@cluster0.xeien5n.mongodb.net/?retryWrites=true&w=majority')
 
-mongoose.connect('mongodb+srv://philip14633:philip14633@cluster0.xeien5n.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URL!)
 .then(() => {
   console.log('connected to Database')
+  
   app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
+   
   })
 })
 
